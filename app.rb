3609bbacdc97b686a6516ae8f109e3d2c10ee0aa
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-18 12:50:49 +0000

    Completed user story 3 - task 22

--- app.rb
+++ app.rb
@@ -17,9 +17,17 @@ class Battle < Sinatra::Base
   get '/play' do
   	@player1_name = session[:player1_name]
   	@player2_name = session[:player2_name]
+    @confirm = session[:confirm]
     @player1_points = 0
     @player2_points = 0
   	erb(:play)
+  end
+
+  post '/attack' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
+    session[:confirm] = "#{@player1_name} attacks #{@player2_name}"
+    redirect '/play'
   end
 
   run! if app_file == $0
