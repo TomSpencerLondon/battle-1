14648b2ac4c2334bd9d16daf2b7ccbabd14e750a
Sam Jones <sam.jones494@googlemail.com>
2017-01-18 12:00:05 +0000

    starting challenge 19

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
-    erb(:play)
+    session[:player1_name] = params[:player1_name]
+    session[:player2_name] = params[:player2_name] 
+    redirect to('/play')  
+  end
+
+  get '/play' do
+  	@player1_name = session[:player1_name]
+  	@player2_name = session[:player2_name]
+  	erb(:play)
   end
 
   run! if app_file == $0
